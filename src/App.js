b2370a61a59b9066369198f9e67da182a7aca6f9
Brandon Cruz <brandonwilliams1993@yahoo.com>
2025-01-24 21:14:42 -0800

    Building Out the Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/frames' element={<ShopCategory category="frame"/>}/>
+        <Route path='/bars' element={<ShopCategory category="bar"/>}/>
+        <Route path='/seats' element={<ShopCategory category="seat"/>}/>
+        <Route path="/product" element={<Product/>}>
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
